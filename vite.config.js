12a1903053efaeb79436398cf61bd81711174b6e
DanNoble <danielwai999@gmail.com>
2024-02-15 04:34:57 -0500

    fixing vite build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  // base: "/DanNoble-Noc/",
+  base: "/Nature-of-code-Blog/",
   build: {
     outDir: "../docs",
     rollupOptions: {
