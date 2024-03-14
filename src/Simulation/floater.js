aacee3abaf0fad196cbdbcf76e98dc194fddf99e
DanNoble <danielwai999@gmail.com>
2024-03-14 07:24:00 -0400

    Midterm Final patch 1.0
    Works with flow field, boundaries and attraction
    
    Food will decay over time

--- src/Simulation/floater.js
+++ src/Simulation/floater.js
@@ -1,33 +1,37 @@
 import * as THREE from "three";
 import { createNoise4D } from "simplex-noise";
 import alea from "alea";
+import { limit } from "./utils";
 
 export class Orb {
-  constructor(x, y, z, m) {
+  constructor(x, y, z, m, s, l) {
     this.pos = new THREE.Vector3(x, y, z);
     this.vel = new THREE.Vector3();
     this.acc = new THREE.Vector3();
     this.mass = m;
+    this.maxSpeed = s;
+    this.maxForce = 3;
+    this.life = l;
   }
 
-  steer(desired) {
-    // let force = new THREE.Vector3();
-    // force.subVectors(target, this.pos);
+  steer(target) {
+    let desired = new THREE.Vector3().subVectors(target, this.pos);
+    desired.normalize();
+    desired.multiplyScalar(this.maxSpeed / 2);
+    let steer = new THREE.Vector3().subVectors(desired, this.vel);
+    //limit(steer, this.maxForce);
+    //console.log(force);
 
-    // // force.setMag(this.maxSpeed);
-    // force.normalize();
-    // force.multiplyScalar(2);
-    // force.sub(this.vel);
-    /// console.log(force);
-    // force.limit(this.maxForce);
-    desired.sub(this.vel);
-    // limit to maxforce
-    this.applyForce(desired);
+    this.applyForce(target);
   }
 
   applyForce(iF) {
     iF.divideScalar(this.mass);
+    //console.log(iF);
     this.acc.add(iF);
+  }
+
+  update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc = new THREE.Vector3(0, 0, 0);
@@ -46,15 +50,15 @@ export class Orb {
   // }
 
   follow(noise3D) {
-    var x = this.pos.x / 100;
-    var y = this.pos.y / 100;
-    var z = this.pos.z / 100;
+    var x = this.pos.x * 10;
+    var y = this.pos.y * 10;
+    var z = this.pos.z * 10;
 
     //Use 2nd noise for two different angles
     // Angle 1 & Angle 2
 
-    var angle = noise3D(x, y, z, 0.5);
-    var angle2 = noise3D(x, y, z, 1);
+    var angle = noise3D(x, y, z, 1);
+    var angle2 = noise3D(x, y, z, 2);
     let noiseDirection = new THREE.Vector3(
       Math.cos(angle * Math.PI) * Math.sin(angle2 * Math.PI),
       Math.sin(angle * Math.PI) * Math.cos(angle2 * Math.PI),
@@ -65,7 +69,7 @@ export class Orb {
     // instead of applyForce
     // return noiseDirection;
 
-    //noiseDirection.divideScalar(10);
+    noiseDirection.divideScalar(20);
     //noiseDirection.sub(this.vel);
     this.applyForce(noiseDirection);
     //this.seek(noiseDirection);
@@ -89,5 +93,38 @@ export class Orb {
 
     // return force;
     orb.steer(force);
+  }
+
+  boundaries(offset) {
+    let desired = new THREE.Vector3();
+
+    if (this.pos.x > offset) {
+      desired.set(-this.maxSpeed, this.vel.y, this.vel.z);
+    }
+    if (this.pos.y > offset) {
+      desired.set(this.vel.x, -this.maxSpeed, this.vel.z);
+    }
+    if (this.pos.z > offset) {
+      desired.set(this.vel.x, this.vel.y, -this.maxSpeed);
+    }
+
+    if (this.pos.x < 0) {
+      desired.set(this.maxSpeed, this.vel.y, this.vel.z);
+    }
+    if (this.pos.y < 0) {
+      desired.set(this.vel.x, this.maxSpeed, this.vel.z);
+    }
+    if (this.pos.z < 0) {
+      desired.set(this.vel.x, this.vel.y, this.maxSpeed);
+    }
+
+    this.applyForce(desired);
+
+    // if (desired !== null) {
+    //   desired.normalize();
+    //   desired.multiplyScalar(this.maxSpeed);
+    //   let force = new THREE.Vector3().subVectors(desired, this.vel);
+    //   this.applyForce(force);
+    // }
   }
 }
--- src/Simulation/main.js
+++ src/Simulation/main.js
@@ -13,6 +13,7 @@ SETUP //////////////////////////////////////////////////////////////////////////
 */
 
 let objects = [];
+let climit = 150;
 
 // app
 const app = document.querySelector("#app");
@@ -39,7 +40,7 @@ const camera = new THREE.PerspectiveCamera(
   1,
   1000
 );
-camera.position.set(20, 50, 200);
+camera.position.set(80, 100, 200);
 
 // axis helper -> X: red, Y: green, Z: blue
 // const axesHelper = new THREE.AxesHelper(5);
@@ -58,13 +59,14 @@ const orbitControls = new OrbitControls(camera, renderer.domElement);
 orbitControls.enableDamping = true;
 orbitControls.dampingFactor = 0.05;
 orbitControls.screenSpacePanning = false;
-orbitControls.enableRotate = true;
+orbitControls.enableRotate = false;
 orbitControls.rotateSpeed = 0.5;
 orbitControls.enableZoom = true;
 orbitControls.zoomSpeed = 0.5;
 orbitControls.minDistance = 100;
 orbitControls.maxDistance = 800;
-orbitControls.target = new THREE.Vector3(0, 0, 0);
+orbitControls.target = new THREE.Vector3(climit / 2, climit / 2, climit / 2);
+orbitControls.autoRotate = true;
 const dragControls = new DragControls(objects, camera, renderer.domElement);
 
 //Disbale orbitControl when dragControl is activated
@@ -95,9 +97,8 @@ const planeNormal = new THREE.Vector3();
 const plane = new THREE.Plane();
 const raycaster = new THREE.Raycaster();
 let Foods = [];
-let foodsDraw = [];
 
-window.addEventListener("keydown", function (e) {
+window.addEventListener("click", function (e) {
   mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
   planeNormal.copy(camera.position).normalize();
@@ -107,7 +108,7 @@ window.addEventListener("keydown", function (e) {
   const sphereMesh = new THREE.Mesh(
     new THREE.SphereGeometry(2, 30, 30),
     new THREE.MeshStandardMaterial({
-      color: 0xf5b7b1,
+      color: 0x64e986,
       metalness: 0,
       roughness: 0,
     })
@@ -118,17 +119,20 @@ window.addEventListener("keydown", function (e) {
     intersectionPoint.x,
     intersectionPoint.y,
     intersectionPoint.z,
-    200 //Mass
+    20, //Mass
+    0,
+    500
   );
   Foods.push(food);
   sphereMesh.position.copy(intersectionPoint);
 });
 
 //Sphere rendering properties
-const sphereMaterial = new THREE.MeshToonMaterial({ color: "#fcc6c2" });
+const sphereMaterial = new THREE.MeshToonMaterial({ color: "#FF9843" });
 // sphereMaterial.transparent = true;
 // sphereMaterial.opacity = 0;
-const sphereGeometry = new THREE.SphereGeometry(0.5, 64, 64);
+//const sphereGeometry = new THREE.SphereGeometry(0.5, 64, 64);
+const sphereGeometry = new THREE.BoxGeometry(1, 1, 1);
 
 let balls = [];
 let fish = [];
@@ -140,13 +144,14 @@ const trailHeadGeometry = [];
 //create spheres
 for (let i = 0; i < 100; i++) {
   fish[i] = new Orb(
-    Math.random() * 1000,
-    Math.random() * 1000,
-    Math.random() * 1000,
-    Math.random() * 5 //Mass
+    Math.random() * climit,
+    Math.random() * climit,
+    Math.random() * climit,
+    Math.random() * 5, //Mass
+    1, //maxSpeed
+    100 //lifetime
   );
   balls[i] = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  balls[i].position.set(fish[i].pos.x, fish[i].pos.y, fish[i].pos.z);
   scene.add(balls[i]);
 
   //////// Create Trail
@@ -164,13 +169,13 @@ function newTrail(i) {
   trails[i] = new TrailRenderer(scene, false);
 
   // set how often a new trail node will be added and existing nodes will be updated
-  trails[i].setAdvanceFrequency(50);
+  trails[i].setAdvanceFrequency(200);
 
   // create material for the trail renderer
   const trailMaterial = TrailRenderer.createBaseMaterial();
 
   // specify length of trail
-  const trailLength = 50;
+  const trailLength = 30;
 
   // initialize the trail
   trails[i].initialize(
@@ -183,8 +188,18 @@ function newTrail(i) {
   );
 
   // activate the trail
-  trailMaterial.uniforms.headColor.value.set(0.1, 0.4, 0.9, 1);
-  trailMaterial.uniforms.tailColor.value.set(0.2, 0.2, 0.9, 0.5);
+  trailMaterial.uniforms.headColor.value.set(
+    1,
+    0.596078431372549,
+    0.2627450980392157,
+    1
+  ); //FF9843
+  trailMaterial.uniforms.tailColor.value.set(
+    1,
+    0.8666666666666667,
+    0.5843137254901961,
+    1
+  ); //FF9843
   trails[i].activate();
 }
 
@@ -193,73 +208,51 @@ function newTrail(i) {
 //Reposition self if position is too high and move somewhere else
 
 let noise4D = createNoise4D();
-let climit = 1000;
+
 let center = new THREE.Vector3(climit, climit, climit);
-let maxSpeed = 3;
 
 const animate = () => {
   requestAnimationFrame(animate);
+
+  for (let h = 0; h < Foods.length; h++) {
+    Foods[h].life--;
+    console.log(Foods[h].life);
+  }
 
   for (let i = 0; i < fish.length; i++) {
     if (Foods.length > 0) {
       for (let h = 0; h < Foods.length; h++) {
         Foods[h].attract(fish[i]);
         Foods[h].pos = objects[h].position;
+        if (Foods[h].life < 0) {
+          let r = Foods.splice(h, 1);
+          scene.remove(objects[h]);
+          let s = objects.splice(h, 1);
+        }
       }
+    } else {
+      fish[i].follow(noise4D);
     }
-    fish[i].follow(noise4D);
 
     // fish[i].applyBehaviors(Foods, noise4D);
 
     //Create boundary
-    if (fish[i].pos.x > climit) {
-      fish[i].pos.x = 0;
-      trails[i].deactivate();
-      newTrail(i);
-    }
-    if (fish[i].pos.y > climit) {
-      fish[i].pos.y = 0;
-      trails[i].deactivate();
-      newTrail(i);
-    }
-    if (fish[i].pos.z > climit) {
-      fish[i].pos.z = 0;
-      trails[i].deactivate();
-      newTrail(i);
-    }
-
-    if (fish[i].pos.x < 0) {
-      fish[i].pos.x = climit;
-      trails[i].deactivate();
-      newTrail(i);
-    }
-    if (fish[i].pos.y < 0) {
-      fish[i].pos.y = climit;
-      trails[i].deactivate();
-      newTrail(i);
-    }
-    if (fish[i].pos.z < 0) {
-      fish[i].pos.z = climit;
-      trails[i].deactivate();
-      newTrail(i);
-    }
+    fish[i].boundaries(climit);
 
     //force field around the area
     //caculate surface of the circle and redirect if moving towards the edge
-    if (fish[i].pos.distanceTo(center) > climit) {
-    }
+    // if (fish[i].pos.distanceTo(center) > climit) {
+    // }
 
     //Limit velocity
-    limit(fish[i].vel, maxSpeed);
+    fish[i].vel = limit(fish[i].vel, fish[i].maxSpeed);
+    fish[i].update();
 
-    balls[i].position.set(
-      fish[i].pos.x / 10,
-      fish[i].pos.y / 10,
-      fish[i].pos.z / 10
-    );
+    balls[i].position.set(fish[i].pos.x, fish[i].pos.y, fish[i].pos.z);
     trails[i].update();
   }
   renderer.render(scene, camera);
+  orbitControls.update();
 };
 
 animate();
